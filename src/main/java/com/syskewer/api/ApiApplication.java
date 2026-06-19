fc9ff08e8cabb83b548c13efac1c839a6fe632dc
CK <charleskauasantanabarbosa@gmail.com>
2026-06-19 00:08:01 -0300

    a

--- src/main/java/com/syskewer/api/ApiApplication.java
+++ src/main/java/com/syskewer/api/ApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 
 @OpenAPIDefinition(info = @Info(title = "Syskewer API", version = "1.0", description = "API de Gestão para Bares"))
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
